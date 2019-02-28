be3659d44fbea237720bcc57b4a83889f02b7288
Cristian Carreño Navarro <cristiancarrenonavarro@cristians-imac.local>
2019-02-28 03:12:53 -0800

    Initial Commit

---
+++ TableView with Details/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView with Details
+//
+//  Created by Cristian Carreño Navarro on 28/02/2019.
+//  Copyright © 2019 Cristian Carreño Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TableView with DetailsTests/TableView_with_DetailsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableView_with_DetailsTests.swift
+//  TableView with DetailsTests
+//
+//  Created by Cristian Carreño Navarro on 28/02/2019.
+//  Copyright © 2019 Cristian Carreño Navarro. All rights reserved.
+//
+
+import XCTest
+@testable import TableView_with_Details
+
+class TableView_with_DetailsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
